57476b2749fc7af17b23365359722ef187ec31ae
Gedeon d' Abreu de Paulo <g.dabreudepaulo-1@student.tudelft.nl>
2022-05-14 18:39:31 +0200

    Streamline handling of tumor status column

--- loader.py
+++ loader.py
@@ -15,18 +15,15 @@ def loadGEWithClinical():
     aak_ge["patient_id"] = aak_ge.apply(lambda row: row.name[:-3], axis=1)   
     aak_ge_and_clinical = aak_ge.join(aak_ge_clinical_types, on=["patient_id"], how="inner")
 
-    aak_ge_and_clinical["tumor"] = aak_ge_and_clinical.apply(lambda row: 1 if re.search(r"[0].$",row.name) else 0, axis=1)
     final = aak_ge_and_clinical.drop(["patient_id"], axis=1).rename(columns={"acronym":"project"})
     return final
 
 def loadTCMA(tcma_type):
     all_cancer_tcma = pd.read_csv(f'Data/TCMA/all_cancers_{tcma_type}.csv', index_col=0)
-    all_cancer_tcma["tumor"] = all_cancer_tcma.apply(lambda row: 1 if re.search(r"[0]..$",row.name) else 0, axis=1)
     return all_cancer_tcma
 
 def loadGEOverlappingTCMA(tcma_type):
     overlapping = pd.read_csv(f'Data/TCMA/all_cancers_{tcma_type}_ge(aakash).csv', index_col=0)
-    overlapping["tumor"] = overlapping.apply(lambda row: 1 if re.search(r"[0].$",row.name) else 0, axis=1)
     return overlapping
 
 # Create data set overlapping TCMA data and GE (Aak)
@@ -38,9 +35,11 @@ def createGEOverlappingTCMA(tcma_type):
 
     tcma_ge = all_cancer_tcma.join(ge, how="inner")
     tcma_ge.index.name = "patient"
-    tcma_ge["tumor"] = tcma_ge.apply(lambda row: 1 if re.search(r"[0].$",row.name) else 0, axis=1)
     tcma_ge.to_csv(f"Data/TCMA/all_cancers_{tcma_type}_ge(aakash).csv")
 
+def attachTumorStatus(pd):
+    pd["tumor"] = pd.apply(lambda row: 1 if re.search(r"[0][0-9][a-zA-Z]?$",row.name) else 0, axis=1)
+    
 if __name__ == "__main__":
 
     createGEOverlappingTCMA("phylum")
