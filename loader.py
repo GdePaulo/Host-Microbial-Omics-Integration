f6b35236798c357537c9e5dc55d7606c8a18b8f9
Gedeon d' Abreu de Paulo <g.dabreudepaulo-1@student.tudelft.nl>
2022-05-14 19:49:14 +0200

    Update file paths, delete unnecessary code and fix scatterplot legend

--- loader.py
+++ loader.py
@@ -3,14 +3,14 @@ import re
 
 
 def loadGE():
-    return pd.read_csv('Data/HNSC/tcga_scaled01_rnaseq.tsv', delimiter = "\t", index_col=0)
+    return pd.read_csv('Data/Aak_Study/tcga_scaled01_rnaseq.tsv', delimiter = "\t", index_col=0)
 
 # Load aakash gene expression data with project
 def loadGEWithClinical():
-    aak_ge_clinical = pd.read_csv('Data/GE(Aakash)_Clinical_Data.tsv', delimiter = "\t", index_col=0)
+    aak_ge_clinical = pd.read_csv('Data/Aak_Study/GE(Aakash)_Clinical_Data.tsv', delimiter = "\t", index_col=0)
     aak_ge_clinical_types = aak_ge_clinical[["acronym"]]
 
-    aak_ge = pd.read_csv('Data/HNSC/tcga_scaled01_rnaseq.tsv', delimiter = "\t", index_col=0)
+    aak_ge = pd.read_csv('Data/Aak_Study/tcga_scaled01_rnaseq.tsv', delimiter = "\t", index_col=0)
 
     aak_ge["patient_id"] = aak_ge.apply(lambda row: row.name[:-3], axis=1)   
     aak_ge_and_clinical = aak_ge.join(aak_ge_clinical_types, on=["patient_id"], how="inner")
@@ -23,7 +23,7 @@ def loadTCMA(tcma_type):
     return all_cancer_tcma
 
 def loadGEOverlappingTCMA(tcma_type):
-    overlapping = pd.read_csv(f'Data/TCMA/all_cancers_{tcma_type}_ge(aakash).csv', index_col=0)
+    overlapping = pd.read_csv(f'Data/Integration/all_cancers_{tcma_type}_ge(aakash).csv', index_col=0)
     return overlapping
 
 # Create data set overlapping TCMA data and GE (Aak)
@@ -35,7 +35,7 @@ def createGEOverlappingTCMA(tcma_type):
 
     tcma_ge = all_cancer_tcma.join(ge, how="inner")
     tcma_ge.index.name = "patient"
-    tcma_ge.to_csv(f"Data/TCMA/all_cancers_{tcma_type}_ge(aakash).csv")
+    tcma_ge.to_csv(f"Data/Integration/all_cancers_{tcma_type}_ge(aakash).csv")
 
 def attachTumorStatus(pd):
     pd["tumor"] = pd.apply(lambda row: 1 if re.search(r"[0][0-9][a-zA-Z]?$",row.name) else 0, axis=1)
