4cf32e0023093cdb498fc81e9de39bb8ed042460
Gedeon d' Abreu de Paulo <g.dabreudepaulo-1@student.tudelft.nl>
2022-05-16 03:46:42 +0200

    Fix duplicates in generated data involving GE (Aak) and Clinical

--- loader.py
+++ loader.py
@@ -9,19 +9,31 @@ def loadGE():
 def loadGEWithClinical(includeStage=False):
     aak_ge_clinical = pd.read_csv('Data/Aak_Study/GE(Aakash)_Clinical_Data.tsv', delimiter = "\t", index_col=0)
     
-    clinical_columns = ["acronym"]
+    clinical_columns = ["acronym", "portion_id"]
     if includeStage:
         
         clinical_columns.append("stage")
 
     aak_ge_clinical_types = aak_ge_clinical[clinical_columns]
-
+    print("Types:",aak_ge_clinical_types.dtypes)
     aak_ge = pd.read_csv('Data/Aak_Study/tcga_scaled01_rnaseq.tsv', delimiter = "\t", index_col=0)
 
-    aak_ge["patient_id"] = aak_ge.apply(lambda row: row.name[:-3], axis=1)   
-    aak_ge_and_clinical = aak_ge.join(aak_ge_clinical_types, on=["patient_id"], how="inner")
+    # There are duplicates also in portion_id eg different measuring tech
+    # aak_ge["patient_id"] = aak_ge.apply(lambda row: row.portion_id[:-4], axis=1)   
+    aak_ge_clinical_types["portion_id"] = aak_ge_clinical_types.apply(lambda row: str(row.portion_id)[:-4], axis=1)   
+    aak_ge_clinical_types.drop_duplicates(["portion_id"], inplace=True)
+    # TCGA-AA-3495-01
 
-    final = aak_ge_and_clinical.drop(["patient_id"], axis=1).rename(columns={"acronym":"project"})
+    aak_ge_clinical_types.reset_index(drop=True, inplace=True)
+    # IMPORTANT to prevent dupes
+    aak_ge_clinical_types.set_index("portion_id", inplace=True)
+    # print(aak_ge_clinical_types.columns)
+    # aak_ge_clinical_types.drop(["sample_id"], axis=1, inplace=True)
+    print("clityps:", aak_ge_clinical_types.head(4))
+    print("reg:", aak_ge.head(4))
+    aak_ge_and_clinical = aak_ge.join(aak_ge_clinical_types, how="left")
+
+    final = aak_ge_and_clinical.rename(columns={"acronym":"project"})
     return final
 
 def loadTCMA(tcma_type):
