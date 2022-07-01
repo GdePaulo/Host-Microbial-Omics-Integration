fb6f161af8a4f64ba6bc130a30525b3114d8833b
Gedeon d' Abreu de Paulo <g.dabreudepaulo-1@student.tudelft.nl>
2022-07-02 01:09:46 +0200

    Add prediction csvs, pr-auc for tumor and fix minimum class requirement

--- predictor.py
+++ predictor.py
@@ -1,8 +1,9 @@
 import imp
+from operator import index
 from sklearn.model_selection import train_test_split, StratifiedKFold
 from sklearn.linear_model import LogisticRegression
 from sklearn.svm import SVC
-from sklearn.metrics import accuracy_score, classification_report
+from sklearn.metrics import accuracy_score, classification_report, average_precision_score
 import loader as load
 import processor as pr
 import pandas as pd
@@ -25,15 +26,19 @@ def runCrossValidation(x, y, splits=2, model="SVC"):
         model.fit(x_train, y_train)
         y_predicted = model.predict(x_test)
 
-        cur_report = classification_report(y_test, y_predicted, output_dict=True)
-
+        cur_report = classification_report(y_test, y_predicted, output_dict=True, zero_division=0)
+        # print(cur_report, " -- sum: ", sum_report)
         for metric in ["precision", "recall", "f1-score"]:
             sum_report[metric] = sum_report.get(metric, 0) + cur_report["macro avg"][metric] / splits
+        
+        if model != "MLGR":
+            sum_report["pr-auc"] = sum_report.get("pr-auc", 0) +  average_precision_score(y_test, y_predicted) / splits
+        print(f"Real:{y_test.values}\nPred:{y_predicted}\nScore:{average_precision_score(y_test, y_predicted)}")
         for k in cur_report.keys():
             if k == "accuracy":
                 break
             sum_report[f"support-{k}"] = sum_report.get(f"support-{k}", 0) + cur_report[k]["support"] / splits
-        sum_report["accuracy"] = sum_report.get("accuracy", 0) + cur_report["accuracy"] / splits
+
     return sum_report    
 
 def runExperiments(data, files, target="tumor", ps=[0, 5, 10]):
@@ -56,8 +61,12 @@ def runExperiments(data, files, target="tumor", ps=[0, 5, 10]):
 
                 x, y = pr.splitData(d, target=target, project=c)
 
-                least_class = y.value_counts(ascending=True).iloc[0]
-                if least_class < 2:
+                # Every class must have at least 2 samples
+                # Also, there must be at least two classes for prediction 
+                val_counts = y.value_counts(ascending=True)
+                least_class = val_counts.iloc[0]
+
+                if least_class < 2 or len(val_counts) < 2:
                     print(f"Skipping {files[i]} {c} {p} {len(best_x)} due to {least_class} least class")
                     continue
                 
@@ -80,5 +89,6 @@ def runExperiments(data, files, target="tumor", ps=[0, 5, 10]):
                 else:
                     final_reports = report_d
         file_name = fr'Data\Descriptor\Prediction_Tables\{target}\{files[i]}_pred.txt'
+        final_reports.to_csv(fr"Data\Descriptor\Prediction_Tables\{target}\{files[i]}_pred.csv", index=None)
         pt = load.getPrettyTable(final_reports)
         load.saveDescriptor(pt, file_name)
