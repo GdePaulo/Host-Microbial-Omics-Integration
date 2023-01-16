0ced09a84dbb486d25613c42938f6dae1d5fa625
Gedeon d' Abreu de Paulo <g.dabreudepaulo-1@student.tudelft.nl>
2023-01-16 20:59:15 +0100

    Add visualization packages for different modality combinations

--- config.py
+++ config.py
@@ -15,3 +15,18 @@ feature_amounts = [0, 6, 10, 26, 50, 100, 200]
 modality_parities = ["imparity", "parity"]
 
 model_state_path = "aidata/model_state"
+
+visualization_packages = {
+    # "all": ['aak_ge', 'tcma_gen', 'tcma_gen_aak_ge', "tcma_gen_aak_ge(parity)"],
+    "base": ['aak_ge', 'tcma_gen', 'tcma_gen_aak_ge'],
+    "base&parity": ['aak_ge', 'tcma_gen', 'tcma_gen_aak_ge', "tcma_gen_aak_ge(parity)"],
+    "base&ae": ['aak_ge', 'tcma_gen', 'tcma_gen_aak_ge', "tcma_gen_aak_ge_ae"],
+}
+
+modality_file_name_to_name = {
+    'aak_ge': "GE",
+    'tcma_gen': "GENUS",
+    'tcma_gen_aak_ge': "GE ∩ GENUS",
+    'tcma_gen_aak_ge(parity)': "GE ∩ GENUS (parity)",
+    'tcma_gen_aak_ge_ae': "GE ∩ GENUS (ae)",
+}
